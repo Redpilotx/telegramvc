172937116c2c935440a3c2ceb6ce65a2a9d2ce8d
Butters Stotch <55245254+Butterstoch@users.noreply.github.com>
2021-04-17 23:57:53 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+
 API_ID = 14371
 API_HASH = "e46b6c854d2bf58a0"
 SUDO_CHAT_ID = -1001485876964  # Chat where the bot will play the music.
 SUDOERS = [1243703097, 13216546] # Users which have special control over the bot.
 
-=======
+
 HEROKU = True   # NOTE Make it false if you're not deploying on heroku.
 
 # NOTE these values are for heroku.
@@ -24,6 +24,6 @@ if not HEROKU:
     OWNER_ID = 1243703097
 
 
->>>>>>> 5a50ddc (Heroku Support (#31))
+
 # don't make changes below this line
 ARQ_API = "https://thearq.tech"
